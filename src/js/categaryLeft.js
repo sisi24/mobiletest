cca8517b6d9a7dedef356158f505891fa8f09597
HeSiqiang <sisihe_24@163.com>
2017-03-18 07:46:16 +0800

    提交categaryLeft

---
+++ src/js/categaryLeft.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Administrator on 2017/3/17.
+ */
+define(function(require , exports , module ){
+    module.exports=function(){
+        var categaryLeft = document.getElementsByClassName("categary-left");
+        var categaryList = document.getElementsByClassName("categary-list");
+        var categaryLeftHeight = categaryLeft[0].offsetHeight;
+        var categaryListHeight = categaryList[0].offsetHeight;
+        var HeightDistance = categaryListHeight-categaryLeftHeight;
+        var touchstartClientY = 0;
+        var touchmoveDistance = 0;
+        var touchendClienY = 0;
+        function moveTransition( isTransition ){
+            categaryList[0].style.transition = isTransition;
+            categaryList[0].style.webkitTransition = isTransition;
+        }
+        function moveTransform( ismoveTransform ){
+            categaryList[0].style.transform = "translateY("+(ismoveTransform)+"px)";
+            categaryList[0].style.webkitTransform = "translateY("+(ismoveTransform)+"px)";
+        }
+        categaryList[0].addEventListener("touchstart",function( e ){
+            touchstartClientY = e.touches[0].clientY;
+        });
+        categaryList[0].addEventListener("touchmove" , function( e ){
+            touchmoveDistance = e.touches[0].clientY - touchstartClientY;
+            if( touchendClienY+touchmoveDistance > 100 ){
+                touchendClienY = 100;
+                moveTransition( "all 0.2s" );
+                moveTransform( touchendClienY );
+            }else if( touchendClienY+touchmoveDistance < (-HeightDistance+-100) ){
+                touchendClienY = (-HeightDistance+-100);
+                moveTransition( "all 0.2s" );
+                moveTransform( touchendClienY );
+            }else{
+                moveTransition( "all 0.2s" );
+                moveTransform( touchendClienY+touchmoveDistance );
+            }
+
+        });
+        categaryList[0].addEventListener("touchend" , function( e ){
+            touchendClienY +=touchmoveDistance;
+            if( touchendClienY > 0 ){
+                touchendClienY = 0;
+                moveTransition( "all 0.2s" );
+                moveTransform( touchendClienY );
+            }else if( touchendClienY < -HeightDistance ){
+                touchendClienY = -HeightDistance;
+                moveTransition( "all 0.2s" );
+                moveTransform( touchendClienY );
+            }
+        });
+    }
+})
